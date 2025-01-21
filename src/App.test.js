34c3184748161b0c02f3ba9bcbe5845a7f1605c2
rryunn <rryunn@ajou.ac.kr>
2025-01-21 13:21:51 +0900

    fix: 테스트 환경: router 추가

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom"; // 추가
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
+test("renders the app without crashing", () => {
+  render(
+    <BrowserRouter>
+      {" "}
+      <App />
+    </BrowserRouter>
+  );
+
+  // 예: "learn react"라는 텍스트가 화면에 나타나는지 확인
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
