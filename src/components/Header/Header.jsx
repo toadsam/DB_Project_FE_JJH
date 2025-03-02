5be10877867598ee574b70bbf75f32325c800881
rryunn <rryunn@ajou.ac.kr>
2025-03-02 15:58:20 +0900

    fix: 헤더 디테일 수정

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,8 +2,6 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import * as S from "./Header.styles";
 import logo from "../../asset/img.jpg";
-
-// 아이콘 임포트 (모바일에서만 사용)
 import {
   FaUniversity,
   FaUsers,
@@ -12,7 +10,6 @@ import {
   FaUserAlt,
 } from "react-icons/fa";
 
-// 각 메뉴에 icon 속성을 추가 (모바일 전용)
 const categories = [
   {
     title: "중앙동아리",
@@ -48,7 +45,7 @@ function Header() {
   const [activeCategory, setActiveCategory] = useState(null);
   const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-  const [openDropdown, setOpenDropdown] = useState(null); // 모바일 드롭다운 상태
+  const [openDropdown, setOpenDropdown] = useState(null);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -66,7 +63,6 @@ function Header() {
   };
 
   const handleCategoryClick = (category) => {
-    // "내정보" 클릭 시
     if (category.title === "내정보") {
       if (isMobile) {
         setMobileMenuOpen(false);
@@ -76,7 +72,6 @@ function Header() {
       }
       return;
     }
-    // 하위 항목이 없다면 바로 이동
     if (!category.items && category.navigateTo) {
       if (isMobile) setMobileMenuOpen(false);
       navigate(category.navigateTo);
@@ -92,7 +87,7 @@ function Header() {
 
   const handleMenuIconClick = () => setMobileMenuOpen((prev) => !prev);
 
-  // 모바일 헤더: 로고와 햄버거 아이콘만 보임
+  // === 모바일 헤더 ===
   if (isMobile) {
     return (
       <S.MobileWrapper>
@@ -143,48 +138,52 @@ function Header() {
     );
   }
 
-  // 데스크탑 헤더: 아이콘은 표시하지 않음
+  // === 데스크탑 헤더 ===
   return (
-    <S.Wrapper>
-      {/* 상단 작은 헤더 */}
-      <S.TopBar>
-        <S.TopBarItem>HOME</S.TopBarItem>
-        <S.TopBarItem onClick={() => navigate("/login")}>LOGIN</S.TopBarItem>
-        <S.TopBarItem>PORTAL</S.TopBarItem>
-        <S.TopBarItem>LANGUAGE ▼</S.TopBarItem>
-      </S.TopBar>
+    <S.OuterWrapper>
+      {/* TopBarBlock: 전체 폭으로 줄을 긋고, 내부는 중앙 정렬 */}
+      <S.TopBarBlock>
+        <S.TopBarInner>
+          <S.TopBarItem>HOME</S.TopBarItem>
+          <S.TopBarItem onClick={() => navigate("/login")}>LOGIN</S.TopBarItem>
+          <S.TopBarItem>PORTAL</S.TopBarItem>
+          <S.TopBarItem>LANGUAGE ▼</S.TopBarItem>
+        </S.TopBarInner>
+      </S.TopBarBlock>
 
-      {/* 메인 헤더 */}
-      <S.Container>
-        <S.LogoLink to="/">
-          <S.Logo src={logo} alt="Ajou Logo" />
-        </S.LogoLink>
-        <S.Menu>
-          {categories.map((category, index) => (
-            <S.MenuItem
-              key={index}
-              onMouseEnter={() => handleMouseEnter(index)}
-              onMouseLeave={handleMouseLeave}
-              onClick={() => handleCategoryClick(category)}
-            >
-              <S.Text>{category.title}</S.Text>
-              {activeCategory === index && category.items && (
-                <S.Dropdown>
-                  {category.items.map((item, idx) => (
-                    <S.DropdownItem
-                      key={idx}
-                      onClick={() => handleItemClick(item)}
-                    >
-                      {item.name}
-                    </S.DropdownItem>
-                  ))}
-                </S.Dropdown>
-              )}
-            </S.MenuItem>
-          ))}
-        </S.Menu>
-      </S.Container>
-    </S.Wrapper>
+      {/* ContainerBlock: 전체 폭으로 줄을 긋고, 내부는 중앙 정렬 */}
+      <S.ContainerBlock>
+        <S.ContainerInner>
+          <S.LogoLink to="/">
+            <S.Logo src={logo} alt="Ajou Logo" />
+          </S.LogoLink>
+          <S.Menu>
+            {categories.map((category, index) => (
+              <S.MenuItem
+                key={index}
+                onMouseEnter={() => handleMouseEnter(index)}
+                onMouseLeave={handleMouseLeave}
+                onClick={() => handleCategoryClick(category)}
+              >
+                <S.Text>{category.title}</S.Text>
+                {activeCategory === index && category.items && (
+                  <S.Dropdown>
+                    {category.items.map((item, idx) => (
+                      <S.DropdownItem
+                        key={idx}
+                        onClick={() => handleItemClick(item)}
+                      >
+                        {item.name}
+                      </S.DropdownItem>
+                    ))}
+                  </S.Dropdown>
+                )}
+              </S.MenuItem>
+            ))}
+          </S.Menu>
+        </S.ContainerInner>
+      </S.ContainerBlock>
+    </S.OuterWrapper>
   );
 }
 
