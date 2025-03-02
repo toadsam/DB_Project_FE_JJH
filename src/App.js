641106d2c0b0409dccd64c10cf9b8d0d86a54925
rryunn <rryunn@ajou.ac.kr>
2025-03-02 17:46:10 +0900

    Fix: 모바일일때만 안내문구 보이게 수정

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import React from "react";
 import AppRouter from "./router.jsx";
 import GlobalStyle from "./GlobalStyle";
 import NoticePopup from "./NoticePopup.jsx";
+import styled from "styled-components";
+const MobileOnlyWrapper = styled.div`
+  display: none;
 
+  @media (max-width: 768px) {
+    display: block;
+  }
+`;
+
+// 사용 예
 function App() {
   return (
     <>
-      <GlobalStyle />{" "}
+      <GlobalStyle />
       <div className="App">
-        <NoticePopup />
+        <MobileOnlyWrapper>
+          <NoticePopup />
+        </MobileOnlyWrapper>
         <AppRouter />
       </div>
     </>
